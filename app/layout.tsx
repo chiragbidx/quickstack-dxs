6ca02320e206d5b8542faee1dee684641dd9dd01
chiragbidx <chirag@bidx.ai>
2026-01-13 14:14:56 +0530

    Root Layout: Remove custom bg, use default

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-gray-100 min-h-screen"}>
+      <body className={inter.className + " min-h-screen"}>
         {children}
       </body>
     </html>
